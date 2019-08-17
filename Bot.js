7ebe16fd51aaa09bddf10126324dcf4a40aaf9b6
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-08-17 23:03:30 +0200

    Create Bot.js

---
+++ Bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => { 
+
+});
+
+client.on('message', message => {
+    if (message.content === '!klan') {
+        message.reply('oto twoje zaproszenie...');
+        message.channel.send('Klan w brawl stars: Fire Power');
+        message.channel.send('**Wymagania: 1000 pucharków**');
+        message.channel.send('https://link.brawlstars.com/invite/band/pl?tag=QLJ0LQJR&token=as37zskx');
+        }
+});
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
